18da1c73ef25647c26343294cbfeb2117d221f3c
Gopala <ggopala1904@gmail.com>
2025-04-29 23:13:03 +0530

    commit pagination

--- src/components/todo-list/todo-list.jsx
+++ src/components/todo-list/todo-list.jsx
@@ -3,48 +3,84 @@ import { Checkbox } from "../checkbox/checkbox";
 import { TodosContext } from "../../todo-context";
 import "./todo-list.scss";
 
+const ITEMS_PER_PAGE = 10;
+
 export const TodoList = () => {
   const { todos, setTodos } = React.useContext(TodosContext);
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const totalPages = Math.ceil(todos.length / ITEMS_PER_PAGE);
 
   const handleDelete = (id) => {
-    // Fix the app to delete a task
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
   };
 
   const toggleCheck = (id) => {
-    
-    let PreTodos = todos.map((todo) =>
+    const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, checked: !todo.checked } : todo
     );
-    setTodos(PreTodos);
-
+    setTodos(updatedTodos);
   };
 
-
   const handleKeyUp = (e, id) => {
-    if (e.keyCode === 5) {
+    if (e.key === "Enter") {
       toggleCheck(id);
     }
   };
-  console.log(todos);
+
+  const paginatedTodos = todos.slice(
+    (currentPage - 1) * ITEMS_PER_PAGE,
+    currentPage * ITEMS_PER_PAGE
+  );
+
+  const goToPage = (pageNum) => {
+    if (pageNum >= 1 && pageNum <= totalPages) {
+      setCurrentPage(pageNum);
+    }
+  };
 
   return (
     <div className="todo-list">
       <span className="todo-list-title">Things to do:</span>
       {todos.length ? (
-        <div className="todo-list-content">
-          {todos.map((todoItem) => (
-            <Checkbox
-              key={todoItem.id}
-              label={todoItem.label}
-              checked={todoItem.checked}
-              onClick={() => toggleCheck(todoItem.id)}
-              onKeyUp={(e) => handleKeyUp(e, todoItem.id)}
-              onDelete={() => handleDelete(todoItem.id)}
-            />
-          ))}
-        </div>
+        <>
+          <div className="todo-list-content">
+            {paginatedTodos.map((todoItem) => (
+              <Checkbox
+                key={todoItem.id}
+                label={todoItem.label}
+                checked={todoItem.checked}
+                onClick={() => toggleCheck(todoItem.id)}
+                onKeyUp={(e) => handleKeyUp(e, todoItem.id)}
+                onDelete={() => handleDelete(todoItem.id)}
+              />
+            ))}
+          </div>
+          <div className="pagination">
+            <button
+              onClick={() => goToPage(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Prev
+            </button>
+            {Array.from({ length: totalPages }, (_, i) => (
+              <button
+                key={i + 1}
+                onClick={() => goToPage(i + 1)}
+                className={currentPage === i + 1 ? "active" : ""}
+              >
+                {i + 1}
+              </button>
+            ))}
+            <button
+              onClick={() => goToPage(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
+        </>
       ) : (
         <div className="no-todos">
           Looks like you&apos;re up for a challenge!
